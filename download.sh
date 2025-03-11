c8b3ce5d03e786a0fa5d6dcdf98a2496cddf25a3
Jiawei <jiawei@Jiaweis-MBP.med.yale.internal>
2025-03-11 16:41:29 -0400

    download resources

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ./resources/CADD_GRCh38_v1.7_no_anno
+wget -c https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/whole_genome_SNVs.tsv.gz
+wget -c https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/whole_genome_SNVs.tsv.gz.tbi
+wget -c https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/gnomad.genomes.r4.0.indel.tsv.gz
+wget -c https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/gnomad.genomes.r4.0.indel.tsv.gz.tbi
